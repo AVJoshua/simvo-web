baaed456d9b3b8f4f0f32f8a0c0889d87f4d8873
Joshua Akalihu <akalihujoshua@gmail.com>
2025-10-19 22:31:55 +0100

    Added Craps Game Stats Section

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -1,11 +1,20 @@
+//Craps Main Data
+let crapsUsername = ""
+
+//Craps Game Settings
+const startingMoney = 1000
+const startingRounds = 0
+
 //HTML Element IDs
 const crapsUsernameInput = "craps-username-input"
 const crapsRegistrationPane = "craps-registration-pane"
 const crapsMainSection = "craps-main-section"
+const crapsStatsUsername = "craps-stats-username"
+const crapsStatsMoney = "craps-stats-money"
+const crapsStatsRounds = "craps-stats-rounds"
 
 function registerCrapsPlayer() {
-	let crapsUsername =	document.getElementById(crapsUsernameInput).value
-
+	crapsUsername =	document.getElementById(crapsUsernameInput).value
 
 	// Username validation check
     let firstCharIsDigitRegex = /^[0-9]|[^a-zA-Z0-9_]/g
@@ -14,8 +23,10 @@ function registerCrapsPlayer() {
     } else {
         removeRegistrationPane()
         showMainGameSection()
+        setUpFirstRound ()
+        setMoney (startingMoney)
+        setRounds(startingRounds)
     }
-    
 }
 
 function removeRegistrationPane () {
@@ -24,6 +35,16 @@ function removeRegistrationPane () {
 
 function showMainGameSection () {
     document.getElementById(crapsMainSection).style.display = "block"
-
 }
 
+function setUpFirstRound () {
+    document.getElementById(crapsStatsUsername).innerHTML = crapsUsername
+}
+
+function setMoney (money) {
+    document.getElementById(crapsStatsMoney).innerHTML = money
+}
+
+function setRounds (round) {
+    document.getElementById(crapsStatsRounds).innerHTML = round
+}
